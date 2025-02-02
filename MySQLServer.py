72b582e10fff2b50c10980ecffeea8cbc584a01a
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-02-02 19:26:03 +0100

    Specify mysql.connector.Error for better error handling in database connection

--- MySQLServer.py
+++ MySQLServer.py
@@ -15,7 +15,7 @@ def create_database():
           sql = 'CREATE DATABASE IF NOT EXISTS alx_book_store'
           cursor.execute(sql)
           print("Database 'alx_book_store' created successfully!")
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: Could not connect to MySQL server - {e}")
     
     finally:
