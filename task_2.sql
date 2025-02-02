c67714f90737309b0f44a3fa270915d8ea3fb0d3
Nnamdi Ekechi <ekechinnamdi@gmail.com>
2025-02-02 19:45:54 +0100

    Fix table name casing for Order_Details in SQL script

--- task_2.sql
+++ task_2.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
